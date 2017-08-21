8b1d56b9f2b74b3a8d08d5fb6eee6b7341988c00
Andres Vargas <vargasa@jlab.org>
2017-08-21 12:14:26 -0400

    TH3D Showing the distribution of events as X,Y and Npe change

--- SignalvsYPosition.C
+++ SignalvsYPosition.C
@@ -14,7 +14,7 @@
 #define NXBIN 120
 #define NYBIN 100
 //Signal
-#define SGNBIN 100
+//#define SGNBIN 10
 #define SGNLO 0
 #define SGNHI 50
 
@@ -34,12 +34,15 @@ Int_t SignalvsYPosition(Int_t RunNumber = 929){
   
   //Aerogel Variables 
   TTreeReaderValue<Double_t> fsumNpe(fReader,"P.aero.npeSum");
+  TTreeReaderArray<Double_t> fposNpe(fReader,"P.aero.posNpe");
+  TTreeReaderArray<Double_t> fnegNpe(fReader,"P.aero.negNpe");
 
 
   TCanvas *ch = new TCanvas("ch");
   TH1I *hnTracks = new TH1I("hnTracks",Form("Tracks per event Run:%d",RunNumber),15,0,15);
   TH2D *hxy = new TH2D("hxy",Form("Spacial distribution of events in the Aerogel Detector Run:%d",RunNumber),NXBIN,XLO,XHI,NYBIN,YLO,YHI);
-  TH2D *hNpeY = new TH2D("hNpeY",Form("Signal vs Y Run:%d",RunNumber),NYBIN,YLO,YHI,SGNBIN,SGNLO,SGNHI);
+  TH2D *hNpeY = new TH2D("hNpeY",Form("Signal vs Y Run:%d",RunNumber),NYBIN,YLO,YHI,100,SGNLO,SGNHI);
+  TH3D *hxyNpe = new TH3D("hxyNpe",Form("Spacial distribution of events in the Aerogel Detector Run:%d",RunNumber),12,XLO,XHI,10,YLO,YHI,10,SGNLO,SGNHI);
   
   while(fReader.Next()){
 
@@ -51,26 +54,35 @@ Int_t SignalvsYPosition(Int_t RunNumber = 929){
 	//Projections into the Aerogel detector
 	Float_t xh = fx[0] + fth[0]*ZAERO;
 	Float_t yh = fy[0] + fph[0]*ZAERO;
+
         hxy->Fill(xh, yh);
 	hNpeY->Fill(yh, *fsumNpe);
+	hxyNpe->Fill(xh, yh, *fsumNpe);
+
       }
   }
 
   hNpeY->GetXaxis()->SetTitle("Y-AeroAxis");
   hNpeY->GetYaxis()->SetTitle("Total Npe");
   hNpeY->GetZaxis()->SetTitle("Counts");
-  hNpeY->Draw();
-  ch->Print(Form("SignalVsY_r%d.png",RunNumber));
+  //hNpeY->Draw();
+  //ch->Print(Form("SignalVsY_r%d.png",RunNumber));
   hNpeY->Draw("COLZ");
   ch->Print(Form("SignalVsY_r%d_COL.png",RunNumber));
-  hNpeY->Draw("LEGO");
-  ch->Print(Form("SignalVsY_r%d_LEGO.png",RunNumber));
+  //hNpeY->Draw("LEGO");
+  //ch->Print(Form("SignalVsY_r%d_LEGO.png",RunNumber));
+
+  hxyNpe->GetXaxis()->SetTitle("X-AeroAxis");
+  hxyNpe->GetYaxis()->SetTitle("Y-AeroAxis");
+  hxyNpe->GetZaxis()->SetTitle("fsumNpe");
+  hxyNpe->Draw("BOX2 Z");
+  ch->Print(Form("xyNpe_r%d.png",RunNumber));
 
   hxy->GetXaxis()->SetTitle("X-AeroAxis");
   hxy->GetYaxis()->SetTitle("Y-AeroAxis");
   hxy->GetZaxis()->SetTitle("Counts");
-  hxy->Draw("LEGO");
-  ch->Print(Form("SpacialAerogelDistribution_r%d.png",RunNumber));
+  hxy->Draw("COLZ");
+  ch->Print(Form("xy_r%d.png",RunNumber));
   
   gPad->SetLogy();
   hnTracks->GetXaxis()->SetTitle("Number of Tracks");

c07cb3371d07753038c5223c4a19dcf5af9bbbfd
Andres Vargas <vargasa@cua.edu>
2017-09-14 21:36:07 -0400

    PrintHisto.C allows to print all the histograms contained in a ROOT file 	* PNG Replacement for $ROOTSYS/tutorials/io/loopdir.C

---
+++ PrintHisto.C
@@ -0,0 +1,17 @@
+void PrintHisto (Int_t RunNumber = 929) {
+  
+  TFile *f1 = TFile::Open(Form("Output/%d.root", RunNumber));
+  TIter keyList(f1->GetListOfKeys());
+  TKey *key;
+  TCanvas c1;
+			  
+  while ((key = (TKey*)keyList())) {
+    TClass *cl = gROOT->GetClass(key->GetClassName());
+    if (!cl->InheritsFrom("TH1")) continue;
+    TH1 *h = (TH1*)key->ReadObj();
+    
+    h->Draw();
+    c1.Print(Form("%s.png", h->GetTitle()));
+  }
+
+}
